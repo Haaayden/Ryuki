e9d197f1749251573c17c19b1188a50b446d63a6
Haaayden <brooklyn1984@qq.com>
2018-11-27 14:25:17 +0800

    feat(wukuang/images): 压缩图片

--- config/config.default.js
+++ config/config.default.js
@@ -1,4 +1,6 @@
 module.exports = {
   weatherKey: 'ucmfae66xfbkmy80',
-  weatherID: 'UF80C383B0'
+  weatherID: 'UF80C383B0',
+  wechatPublicAppID: 'wx919c5505efc13932',
+  wechatPublicAppSecret: '851afce10186f0b808472d25fb850617'
 }
